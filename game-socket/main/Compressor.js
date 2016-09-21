a46e7534d0427c9ef343cd3dd3715713e3d550e1
Samvel Abrahamyan <samvel1024@gmail.com>
2016-09-22 00:22:35 +0400

    Added compressor and test

---
+++ game-socket/main/Compressor.js
@@ -0,0 +1,109 @@
+/**
+ * Created by sam on 9/21/16.
+ */
+
+"use strict";
+
+
+var Compressor = function(rows, cols){
+
+    var nearestR = nextBinPow(rows), nearestC = nextBinPow(cols);
+
+    function nextBinPow(n){
+        var ans = 1;
+        while(ans <= n)
+            ans *= 2;
+        return ans;
+    }
+
+    function encodePair(row, col){
+        var res = row;
+        res *= nearestC;
+        res += col;
+        return res;
+    }
+
+
+    function decodePair(encoded){
+        var col = encoded % nearestC;
+        var row = Math.floor(encoded / nearestC);
+        return {row: row, col:col };
+    }
+
+    function responseModel(){
+        return {
+            rot: [],
+            chown: []
+        }
+    }
+
+    function requestModel(){
+        return {row: null, col: null}
+    }
+
+
+
+    return {
+
+        encodeServerResponse: function(obj){
+            var encoded = "";
+            if (obj.rot.length == 0)
+                return encoded;
+
+
+            encoded += String.fromCharCode(encodePair(obj.rot.row, obj.rot.col) );
+            var i;
+            for (i in obj.chown){
+                var code = encodePair(obj.chown[i].row, obj.chown[i].col);
+                encoded += String.fromCharCode(code);
+            }
+
+            return encoded;
+        },
+
+        decodeServerResponse: function(encoded){
+
+            var decoded = responseModel();
+            if (encoded.length == 0)
+                return decoded;
+
+            var rotateTile = decodePair(encoded.charCodeAt(0));
+            decoded.rot = {row: rotateTile.row, col: rotateTile.col};
+
+
+
+            for (var i=1; i<encoded.length; ++i){
+                var chownTile = decodePair(encoded.charCodeAt(i));
+                decoded.chown.push({row: chownTile.row, col: chownTile.col});
+            }
+
+            return decoded;
+        },
+
+        encodeClientRequest: function(obj){
+            var encoded = "";
+
+
+            var clickEncoded = encodePair(obj.row, obj.col);
+            encoded += String.fromCharCode(clickEncoded);
+
+
+
+            return encoded;
+        },
+
+        decodeClientRequest: function(encoded){
+            var obj = requestModel();
+
+            var clickPos = decodePair(encoded.charCodeAt(0));
+            obj.row = clickPos.row;
+            obj.col = clickPos.col;
+
+            return obj;
+        }
+    };
+};
+
+
+
+module.exports = Compressor;
---
+++ game-socket/test/compressor-test.js
@@ -0,0 +1,48 @@
+/**
+ * Created by sam on 9/22/16.
+ */
+
+"use strict";
+var Compressor = require('../main/Compressor');
+var chai = require('chai');
+describe("Compressor test", () => {
+
+    var compressor = Compressor(10, 20);
+
+    it("encode/decode client request", () => {
+        var client = {
+            raw: {
+                row: 2, col: 5
+            },
+            encoded: null
+        };
+        client.encoded = compressor.encodeClientRequest(client.raw);
+        var decoded = compressor.decodeClientRequest(client.encoded);
+        chai.assert.deepEqual(decoded, client.raw);
+    });
+    
+    
+    it("encode/decode server response", () => {
+        var server = {
+            raw: {
+                rot: {
+                    row: 2, col: 5
+                },
+                chown: [
+                    {row: 2, col: 6},
+                    {row: 3, col: 6},
+                    {row: 4, col: 6},
+                    {row: 4, col: 7},
+                    {row: 4, col: 8},
+                    {row: 5, col: 8},
+                    {row: 5, col: 9},
+                    {row: 5, col: 10}
+                ]
+            },
+            encoded: null
+        };
+        server.encoded = compressor.encodeServerResponse(server.raw);
+        var decoded = compressor.decodeServerResponse(server.encoded);
+        chai.assert.deepEqual(decoded, server.raw);
+    });
+});
